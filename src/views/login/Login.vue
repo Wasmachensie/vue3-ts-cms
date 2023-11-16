7e58c26443ee7177a9bf0e04459514be9f38517c
Wasmachensie <simiyx01@163.com>
2023-11-16 23:08:32 +0800

    登录界面背景
    设置登录界面背景，并且让背景有一个磨砂玻璃的特效

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -8,6 +8,21 @@
 
 <style scoped lang="less">
 .login {
-  color: red;
+  height: 100%;
+  width: 100%;
+  background: url('../../assets//img/login-bg.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.login::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(10px);
+  opacity: 0.6;
 }
 </style>
