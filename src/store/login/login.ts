5facfb192009148e11beb319b93959cd2786a8d7
Wasmachensie <simiyx01@163.com>
2023-11-23 22:56:32 +0800

    封装cache方法-处理localStorage
    封装一个cache工具类，用来处理localStorage\sessionStorage方法

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,19 +1,24 @@
 import { accountLogin } from '@/service/login/login'
-import { defineStore } from 'pinia'
 import type { IAccount } from '@/types'
+import { localCache } from '@/utils/cache'
+import { defineStore } from 'pinia'
 const useLoginStore = defineStore('login', {
   state: () => ({
     id: '',
     name: '',
-    token: ''
+    token: localCache.getCache('token') ?? '' // 没有的话就设为空字符串
   }),
   actions: {
     async loginAccountAction(account: IAccount) {
+      // 1.账号登陆，获取token
       const loginResult = await accountLogin(account)
       console.log('res==>', loginResult)
       this.id = loginResult.data.id
       this.name = loginResult.data.name
       this.token = loginResult.data.token
+
+      // 2.进行本地化存储
+      localCache.setCache('token', this.token)
     }
   }
 })
