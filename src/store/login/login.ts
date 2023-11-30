58d91b652dddaca1945232fb04c09b4e2586f056
Wasmachensie <simiyx01@163.com>
2023-11-30 22:11:31 +0800

    1、实现main界面整体布局 2、完成侧边栏功能

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -26,8 +26,8 @@ interface ILoginState {
 const useLoginStore = defineStore('login', {
   state: (): ILoginState => ({
     token: localCache.getCache(LOGIN_TOKEN) ?? '', // 没有的话就设为空字符串,??是非空断言
-    userInfo: {},
-    userMenus: []
+    userInfo: localCache.getCache('userInfo') ?? {},
+    userMenus: localCache.getCache('userMenus') ?? []
   }),
   actions: {
     async loginAccountAction(account: IAccount) {
@@ -38,16 +38,25 @@ const useLoginStore = defineStore('login', {
         const id = loginResult.data.id
         // const name = loginResult.data.name
         this.token = loginResult.data.token
+
         // 2.进行本地化存储
         localCache.setCache(LOGIN_TOKEN, this.token)
+
         // 2.1 获取用户详细信息(role信息)
         const userInfoResult = await getUserInfoById(id)
         this.userInfo = userInfoResult.data
         console.log('resInfo==>', this.userInfo)
+
         // 2.2根据角色请求用户权限(菜单)
         const roleMenuResult = await getUserMenuInfoById(this.userInfo.role?.id)
         console.log('roleMenuResult==>', roleMenuResult)
-        this.userMenus = roleMenuResult.data
+        const userMenus = roleMenuResult.data
+        this.userMenus = userMenus
+
+        // 2.3 进行本地化存储
+        localCache.setCache('userInfo', this.userInfo)
+        localCache.setCache('userMenus', userMenus)
+
         // 3.页面跳转(mian.vue)
         router.push('/main')
       } else {
--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="main">
-    <h2>main</h2>
-    <el-button type="primary">点击实际</el-button>
-    <el-button type="primary" @click="handleLogout">退出登录</el-button>
+    <el-container class="main-content">
+      <el-aside class="main-aside">
+        <MainMenu></MainMenu>
+      </el-aside>
+      <el-container>
+        <el-header>
+          <MainHeader></MainHeader>
+        </el-header>
+        <el-main>
+          <el-button type="primary" @click="handleLogout">退出登录</el-button>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup lang="ts">
+import MainHeader from '@/components/main-header/main-header.vue'
+import MainMenu from '@/components/main-menu/main-menu.vue'
 import { LOGIN_TOKEN } from '@/global/constants'
 import { req, reqCaptchaCode } from '@/service/main/main'
 import { localCache } from '@/utils/cache'
 import { onBeforeMount } from 'vue'
 import { useRouter } from 'vue-router'
-//数据
 
+//数据
 onBeforeMount(async () => {
   let res = await reqCaptchaCode()
   let resData = await req()
@@ -29,6 +41,32 @@ const handleLogout = () => {
 
 <style scoped lang="less">
 .main {
-  color: antiquewhite;
+  height: 100%;
+  width: 100%;
+  .main-content {
+    height: 100%;
+    width: 100%;
+    .el-aside {
+      width: 15vw;
+      overflow-x: hidden;
+      overflow-y: auto;
+      line-height: 200px;
+      text-align: left;
+      cursor: pointer;
+      background-color: #001529;
+      scrollbar-width: none; /* firefox */
+      -ms-overflow-style: none; /* IE 10+ */
+      transition: width 0.3s ease;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+    }
+    .el-header {
+      background-color: antiquewhite;
+    }
+    .el-main {
+      background-color: #f0f2f5;
+    }
+  }
 }
 </style>
