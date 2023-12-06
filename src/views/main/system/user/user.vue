810f41dae3aa6972c16069f7f9bb5d6a1e6d9026
Wasmachensie <simiyx01@163.com>
2023-12-06 12:56:49 +0800

    1、完成系统管理-用户管理头部搭建
    完成系统管理-用户管理头部搭建，以及日期国际化包裹

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="users">
-    <h2>users</h2>
+    <UserSearch />
+    <div class="content">
+      <div class="header">头部</div>
+      <div class="table">表格</div>
+      <div class="pagination">分页</div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UserSearch from './c-cpns/user-search.vue'
+</script>
 
-<style scoped lang="less">
-.users {
-  color: red;
-}
-</style>
+<style scoped lang="less"></style>
